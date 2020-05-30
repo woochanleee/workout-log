ffa12ae50c305faeaadb95420245b1a4f481ec38
_uchanlee <uchanlee.dev@gmail.com>
2020-05-30 10:59:55 +0900

    Add middleware

--- backend/src/api/posts/comments/index.ts
+++ backend/src/api/posts/comments/index.ts
@@ -9,10 +9,9 @@ comments.post("/", commentCtrl.write);
 
 const comment = new Router();
 comment.get("/", commentCtrl.read);
-comment.delete("/", commentCtrl.remove);
-comment.patch("/", commentCtrl.update);
+comment.delete("/", commentCtrl.checkOwnComment, commentCtrl.remove);
+comment.patch("/", commentCtrl.checkOwnComment, commentCtrl.update);
 
-comments.use("/:comment_id", commentCtrl.checkId, comment.routes());
+comments.use("/:comment_id", commentCtrl.getCommentById, comment.routes());
 comments.use("/:comment_id/recomments", recomments.routes());
-
 export default comments;
