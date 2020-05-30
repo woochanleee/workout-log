becbf04fc520e4ebfd8534bf1783280fff6a4c0d
_uchanlee <uchanlee.dev@gmail.com>
2020-05-30 10:57:01 +0900

    Create auth.ctrl.ts

--- backend/src/api/auth/index.ts
+++ backend/src/api/auth/index.ts
@@ -1,10 +1,19 @@
 import Router from 'koa-router';
+import koaBody from 'koa-body';
 import * as authCtrl from './auth.ctrl';
+import checkLoggedIn from '../../lib/checkLoggedIn';
 
 const auth = new Router();
 
 auth.post('/login', authCtrl.login);
 auth.get('/check', authCtrl.check);
-auth.post('/logout', authCtrl.logout);
+auth.post('/logout', checkLoggedIn, authCtrl.logout);
+auth.delete('/leave', checkLoggedIn, authCtrl.leave);
+auth.patch('/user', 
+    koaBody({
+        multipart: true,
+    }),
+    authCtrl.update
+);
 
 export default auth;
