13da3df80a5d684b9491c7f923e409d2ad4e3a84
_uchanlee <uchanlee.dev@gmail.com>
2020-05-30 10:57:01 +0900

    Create auth.ctrl.ts

--- backend/src/api/auth/auth.ctrl.ts
+++ backend/src/api/auth/auth.ctrl.ts
@@ -1,7 +1,12 @@
 import { Context } from 'koa';
 import Joi from 'joi';
+import fs from 'fs';
+import { v1 as uuidv1 } from 'uuid';
+
 import loginTypeList from '../../../../utils/loginTypeList';
 import User from '../../models/user';
+import Post from '../../models/post';
+import { extensionList } from '../../../../utils/extensionList';
 
 /*
     POST /api/auth/login
@@ -13,9 +18,9 @@ import User from '../../models/user';
 */
 export const login = async (ctx: Context) => {
   const schema = Joi.object().keys({
-    username: Joi.string(),
-    email: Joi.string(),
-    profileImage: Joi.string(),
+    username: Joi.string().required(),
+    email: Joi.string().required(),
+    profileImage: Joi.string().required(),
   });
   const result = Joi.validate(ctx.request.body, schema);
   if (result.error) {
@@ -77,4 +82,118 @@ export const check = async (ctx: Context) => {
 export const logout = async (ctx: Context) => {
   ctx.cookies.set('access_token');
   ctx.status = 204;
+};
+
+export const leave = async (ctx: Context) => {
+  const { user } = ctx.state;
+  try {
+    if (user) {
+      await User.findOneAndRemove({ email: user.email });
+      await Post.deleteMany({ 'user.email': user.email }).exec();
+      ctx.cookies.set('access_token');
+      ctx.status = 204;
+      return;
+    }
+    ctx.status = 404;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+}
+
+export const update = async (ctx: any) => {
+  const schema = Joi.object().keys({
+    username: Joi.string().required(),
+    file: Joi.any(),
+  });
+  const result = Joi.validate(
+    {
+      ...ctx.request.body,
+      file: ctx.request.files.file,
+    },
+    schema
+  );
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
+  const { username } = ctx.request.body;
+  const file = ctx.request.files.file;
+  const fileDir = `upload/profileImage`;
+  let profileData: string;
+
+  const updateDatabase = async () => {
+    try {
+      const user = await User.findOneAndUpdate(
+        {
+          email: ctx.state.user.email
+        },
+        {
+          username,
+          profileImage: profileData
+        },
+        {
+          new: true
+        }
+      ).exec();
+      console.log(user, ctx.state.emai);
+      ctx.body = user;
+    } catch (e) {
+      ctx.throw(500, e);
+    }
+  }
+  const profileImage = ctx.state.user.profileImage;
+  if (profileImage.includes('upload/profileImage') && profileImage !== 'upload/profileImage/default.PNG')
+    await deleteFile(profileImage);
+  if (file) {
+    let fileName = uuidv1();
+        let extension = file.name.split(".").slice(-1)[0].toUpperCase();
+        try {
+          while (
+            fs.lstatSync(`public/${fileDir}/${fileName}.${extension}`).isFile()
+          ) {
+            fileName = uuidv1();
+            break;
+          }
+        } catch (e) {
+          console.error(e);
+        }
+  
+        let path = `${fileDir}/${fileName}.${extension}`;
+        if (!extensionList.includes(extension)) {
+          ctx.status = 405;
+          ctx.body = {
+            error: "허용되지 않은 확장자",
+          };
+          return;
+        }
+        await saveFile(file, path)
+          .then(() => (profileData = path))
+          .catch((err) => console.log(err));
+        await updateDatabase();
+  } else {
+    profileData = 'upload/profileImage/default.PNG';
+    await updateDatabase();
+  }
+
+}
+
+const saveFile = (file: any, path: string) => {
+  return new Promise((resolve, reject) => {
+    let render = fs.createReadStream(file.path);
+    let upStream = fs.createWriteStream(`./public/${path}`);
+    render.pipe(upStream);
+    upStream.on("finish", () => {
+      resolve(path);
+    });
+    upStream.on("error", (err) => {
+      reject(err);
+    });
+  });
+};
+
+const deleteFile = (path: string) => {
+  fs.unlink(`./public/${path}`, (err) => {
+    if (err) console.error(err);
+  });
 };
