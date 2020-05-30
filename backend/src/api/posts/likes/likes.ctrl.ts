7c52963b8693eac99e61a823fc82882dcb94d9fd
_uchanlee <uchanlee.dev@gmail.com>
2020-05-30 10:59:44 +0900

    Update likes.ctrl.ts

--- backend/src/api/posts/likes/likes.ctrl.ts
+++ backend/src/api/posts/likes/likes.ctrl.ts
@@ -1,20 +1,31 @@
 import Post from "../../../models/post";
 import { Context } from "koa";
 
-export const like = async (ctx: any) => {
+export const like = async (ctx: Context) => {
   const { id } = ctx.params;
+  const { user } = ctx.state;
   let post: any = await Post.findOne({ id }).exec();
+  const likeUsers: Array<string> = post.likeUsers;
   try {
     if (!post) {
       ctx.status = 404;
       return;
     }
+    if (likeUsers.includes(user.email)) {
+      ctx.status = 403;
+      ctx.body = {
+        error: '좋아요를 이미 눌렀습니다.'
+      }
+      return;
+    }
+    likeUsers.push(user.email);
     post = await Post.findOneAndUpdate(
       {
         id,
       },
       {
-        likes: post.likes + 1,
+        likes: likeUsers.length,
+        likeUsers
       },
       {
         new: true,
@@ -26,12 +37,21 @@ export const like = async (ctx: any) => {
   }
 };
 
-export const notLike = async (ctx: any) => {
+export const notLike = async (ctx: Context) => {
   const { id } = ctx.params;
+  const { user } = ctx.state;
   let post: any = await Post.findOne({ id }).exec();
+  const likeUsers: Array<string> = post.likeUsers.filter((email: string) => email !== user.email);
   try {
     if (!post) {
       ctx.status = 404;
+      return;
+    }
+    if (post.likeUsers.join() === likeUsers.join()) {
+      ctx.status = 403;
+      ctx.body = {
+        error: '좋아요를 누르지 않았습니다.'
+      }
       return;
     }
     post = await Post.findOneAndUpdate(
@@ -39,7 +59,8 @@ export const notLike = async (ctx: any) => {
         id,
       },
       {
-        likes: post.likes - 1,
+        likes: likeUsers.length,
+        likeUsers
       },
       {
         new: true,
