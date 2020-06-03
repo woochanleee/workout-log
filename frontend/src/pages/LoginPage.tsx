e844ad040d4a40939331db3aa4bbce220f3deafc
_uchanlee <uchan.dev@gmail.com>
2020-06-04 08:32:09 +0900

    Create route

---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,6 @@
+import React, { FC } from 'react';
+import { Button } from 'react-bootstrap';
+
+const LoginPage: FC<{}> = () => <Button>로그인</Button>;
+
+export default LoginPage;
---
+++ frontend/src/pages/PostListPage.tsx
@@ -0,0 +1,5 @@
+import React, { FC } from 'react';
+
+const PostListPage: FC<{}> = () => <div>포스트 리스트</div>;
+
+export default PostListPage;
