4a62dbdf93630154923fadeb974d0848e47b5915
_uchanlee <uchan.dev@gmail.com>
2020-06-03 18:28:49 +0900

    Add bootstrap

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -4,47 +4,45 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const port = 3000;
 
 module.exports = {
-    mode: 'development',
-    entry:'./src/index.tsx',
-    output:{
-        path: __dirname + '/dist',
-        filename: 'bundle.js',
-        publicPath: '/'
-    },
-    module: {
-        rules: [
-        {
-            test: /\.(js|ts)$/,
-            exclude: /node_modules/,
-            use: ['babel-loader']
-        },
-        {
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/
-        },
-        {
-            test: /\.css$/,
-            use: [
-                'style-loader',
-                'css-loader'
-            ]
-        }
-    ]
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js']
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'public/index.html',
-        })
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'bundle.js',
+    publicPath: './',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$|jsx/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'], // 오른쪽에서 왼쪽 순서로 실행됨
+      },
     ],
-    devServer: {
-        host: 'localhost',
-        port: port,
-        open: true,
-        historyApiFallback: true,
-        hot: true
-    }
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    host: 'localhost',
+    port,
+    open: true,
+    historyApiFallback: true,
+    hot: true,
+    contentBase: `${__dirname}/dist`,
+  },
 };
