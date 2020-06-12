5dd46a5c23595df980bd0d3dba414541b6e3abba
_uchanlee <uchan.dev@gmail.com>
2020-06-12 20:23:27 +0900

    Update middleware

--- backend/src/api/posts/comments/index.ts
+++ backend/src/api/posts/comments/index.ts
@@ -1,15 +1,16 @@
 import Router from "koa-router";
 import * as commentCtrl from "./comment.ctrl";
 import recomments from "./recomments";
+import checkLoggedIn from "../../../lib/checkLoggedIn";
 
 const comments = new Router();
 
 comments.get("/", commentCtrl.list);
-comments.post("/", commentCtrl.write);
+comments.post("/", checkLoggedIn, commentCtrl.write);
 
 const comment = new Router();
 comment.get("/", commentCtrl.read);
-comment.delete("/", commentCtrl.checkOwnComment, commentCtrl.remove);
+comment.delete("/", checkLoggedIn, commentCtrl.checkOwnComment, commentCtrl.remove);
 comment.patch("/", commentCtrl.checkOwnComment, commentCtrl.update);
 
 comments.use("/:comment_id", commentCtrl.getCommentById, comment.routes());
