e7e06e055ea60342d03e17811a406c0c261ba22c
_uchanlee <uchan.dev@gmail.com>
2020-06-12 20:23:50 +0900

    Update checkedLogin middleware

--- backend/src/api/posts/comments/recomments/index.ts
+++ backend/src/api/posts/comments/recomments/index.ts
@@ -1,15 +1,16 @@
 import Router from "koa-router";
 import * as recommentCtrl from "./recomment.ctrl";
+import checkLoggedIn from "../../../../lib/checkLoggedIn";
 
 const recomments = new Router();
 
 recomments.get("/", recommentCtrl.list);
-recomments.post("/", recommentCtrl.write);
+recomments.post("/",checkLoggedIn,recommentCtrl.write);
 
 const recomment = new Router();
 
-recomment.delete("/", recommentCtrl.checkOwnRecomment, recommentCtrl.remove);
-recomment.patch("/", recommentCtrl.checkOwnRecomment, recommentCtrl.update);
+recomment.delete("/", checkLoggedIn, recommentCtrl.checkOwnRecomment, recommentCtrl.remove);
+recomment.patch("/", checkLoggedIn, recommentCtrl.checkOwnRecomment, recommentCtrl.update);
 
 recomments.use("/:recomment_id", recommentCtrl.getRecommentById, recomment.routes());
 export default recomments;
