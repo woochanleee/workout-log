fc18878b71bb325cf15e3559041f3b2d08f09377
_uchanlee <uchan.dev@gmail.com>
2020-06-12 20:12:27 +0900

    Update postViewer

--- frontend/src/components/posts/PostViewer.tsx
+++ frontend/src/components/posts/PostViewer.tsx
@@ -7,6 +7,10 @@ import { readPost, removePost } from '../../lib/api/posts';
 import { SubInfo, Tags } from '../common';
 import PostActionButtons from './PostActionButtons';
 import { userState } from '../../modules/auth';
+import { isLike, isNotLike } from '../../../public/assets';
+import { like, notLike } from '../../lib/api/liking';
+import { check } from '../../lib/api/auth';
+import CommentList from './comments/CommentList';
 
 const PostViewerWrapper = styled.div`
   margin-top: 4rem;
@@ -17,12 +21,30 @@ const PostViewerWrapper = styled.div`
     overflow-x: scroll;
     margin-bottom: 1rem;
     white-space: nowrap;
-    > img {
+    > embed {
       object-fit: cover;
       width: 100%;
       height: auto;
       max-height: 31rem;
     }
+  }
+  .additional {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    margin-bottom: 2rem;
+    margin-top: -1.5rem;
+    img {
+      width: 20px;
+      height: 20px;
+      cursor: pointer;
+    }
+    span {
+      margin-right: 4px;
+    }
+  }
+  ul {
+    padding: 0;
   }
 `;
 
@@ -59,10 +81,31 @@ const PostViewer: FC<
   const user = useRecoilValue(userState);
   const [post, setPost] = useRecoilState(postState);
   const [write, setWrite] = useRecoilState(writeState);
+  const [liking, setLiking] = useState(false);
   useEffect(() => {
     readPost(postId)
       .then((res) => {
         setPost(res.data);
+        check()
+          .then((userRes) => {
+            let user = {
+              email: userRes.data.email,
+              loginType: userRes.data.loginType,
+              profileImage: userRes.data.profileImage,
+              username: userRes.data.username,
+              workoutDays: userRes.data.workoutDays,
+            };
+            if (user) {
+              if (
+                res.data.likeUsers.findIndex((e) => e === user.email) === -1
+              ) {
+                setLiking(false);
+              } else {
+                setLiking(true);
+              }
+            } else setLiking(false);
+          })
+          .catch((err) => console.log(err));
       })
       .catch((err) => {
         if (err.response && err.response.status === 404) {
@@ -90,6 +133,27 @@ const PostViewer: FC<
       .then((res) => history.push('/'))
       .catch((err) => console.log(err));
   };
+
+  const onLikeHandler = () => {
+    if (user.username === '') {
+      return alert('로그인 후 이용해주세요.');
+    }
+    if (liking) {
+      notLike(postId)
+        .then((res) => {
+          setPost(res.data);
+          setLiking(false);
+        })
+        .catch((err) => console.log(err));
+    } else {
+      like(postId)
+        .then((res) => {
+          setLiking(true);
+          setPost(res.data);
+        })
+        .catch((err) => console.log(err));
+    }
+  };
   if (error === 'not found')
     return (
       <PostViewerWrapper>
@@ -111,23 +175,29 @@ const PostViewer: FC<
           <h1>{post.title}</h1>
           <SubInfo
             username={post.user.username}
+            profileImage={post.user.profileImage}
             publishedDate={post.publishedDate}
             hasMarginTop
           />
           <Tags tags={post.tags} />
         </PostHead>
-        {(user && user.email) === (post && post.user.email) && (
-          <PostActionButtons onEdit={onEdit} onRemove={onRemove} />
-        )}
+        <div className="additional">
+          <span>{post.likes}</span>
+          <img src={liking ? isLike : isNotLike} onClick={onLikeHandler} />
+          {(user && user.email) === (post && post.user.email) && (
+            <PostActionButtons onEdit={onEdit} onRemove={onRemove} />
+          )}
+        </div>
 
         <div id="files">
           {post.files &&
             post.files.length !== 0 &&
             post.files.map((f) => (
-              <img src={`${process.env.SERVER_URL}/${f}`} key={f} />
+              <embed src={`${process.env.SERVER_URL}/${f}`} key={f} />
             ))}
         </div>
         <PostContent dangerouslySetInnerHTML={{ __html: post.body }} />
+        <CommentList />
       </div>
     </PostViewerWrapper>
   );
